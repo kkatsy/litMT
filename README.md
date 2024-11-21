3c3e4440f870db8f84b140cad5e5aaff22dd0428
kkatsy <kkatsy@ucsd.edu>
2024-11-20 16:05:33 -0800

    added readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# LitMT
+
+## Motivation
+## Acquiring Parallel Data
+## Scanning Kindle Books via Apple Scripts and OCR
+## Aligning Book Translations with Par3
+## Running Classification with mBERT
+## Idiom Dive
+## Style Transfer
+
--- scripts/run_classification_2.py
+++ scripts/run_classification_2.py
@@ -14,17 +14,17 @@ batch_size = 12
 
 # dataset dirs
 dir = "/home/kkatsy/litMT/"
-dataset_dir = dir + "experiment_dataset/"
-aligned_train_file = dataset_dir + '4class_same_holdout/4class_same_holdout_aligned_train_df.pickle'
-random_train_file = dataset_dir + '4class_same_holdout/4class_same_holdout_random_train_df.pickle'
+dataset_dir = dir + "experiment_dataset/experiment_dataset/"
+aligned_train_file = dataset_dir + 'original_clean/aligned_train_df.pickle'
+random_train_file = dataset_dir + 'original_clean/random_train_df.pickle'
 
 train_file = aligned_train_file if alignment=='aligned' else random_train_file
-val_file = dataset_dir + '4class_same_holdout/4class_same_holdout_experiment_val_df.pickle'
-test_file = dataset_dir + '4class_same_holdout/4class_same_holdout_experiment_test_df.pickle'
+val_file = dataset_dir + 'original_clean/experiment_val_df.pickle'
+test_file = dataset_dir + 'original_clean/experiment_test_df.pickle'
 
 # model info
 model = "bert-base-multilingual-cased"
-classes = ["Garnett", "McDuff", "PV", "Katz"]
+classes = ["Garnett", "McDuff", "PV", "Katz", "Hogarth"]
 
 # fine-tune settings
 epochs = 20
@@ -34,9 +34,9 @@ wd = 0.0
 train_args = {'epochs': epochs, 'lr': lr, 'eps': eps, 'wd': wd}
 
 # logging settings
-proj_name = "4 class - same holdout"
-run_name = "aligned tgt512  lr=2e-5 same_holdout"
-model_save_pth = "/projects/kkatsy/pretrained/" + run_name
+proj_name = "original de-unk-ed"
+run_name = "aligned tgt512 lr=2e-5 orig_clean"
+model_save_pth = "/trunk/kkatsy/pretrained/" + run_name
 
 def main():
     train_set, val_set, test_set = preprocess_data(model, train_file, val_file, test_file, context_type, batch_size)
